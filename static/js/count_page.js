27315b7573a10d6d4759060d91d3a21494ed6324
Greg Dolder <greg.dolder@specialized.com>
2016-08-23 17:41:05 -0700

    styling updates icons font-awesome stuff.

--- static/js/count_page.js
+++ static/js/count_page.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         type: "get",
         async: false,
-        url: "http://jalpc-a.leanapp.cn/api/jalpc_count",
+        url: "https://jalpc-a.leanapp.cn/api/jalpc_count",
         dataType: "jsonp",
         jsonp: "callback",
         jsonpCallback: "flightHandler",
